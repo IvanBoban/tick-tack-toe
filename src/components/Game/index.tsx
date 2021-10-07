ab6e8b7e48670fdc5eb1a0e218570631066e7c4f
Ivan Boban <ivan.boban911@gmail.com>
2021-10-07 19:16:24 +0200

    Added move number tracker

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -4,11 +4,11 @@ import "./Game.css";
 import useGame from "./useGame";
 
 export default function Game() {
-  const { currentPlayer } = useGame();
-  //to do map out squares onto game
+  const { currentPlayer, moveNumber } = useGame();
   return (
     <div id="game">
       <h3>Current player: {currentPlayer} </h3>
+      <h4>Number of moves: {moveNumber}</h4>
       <Board />
     </div>
   );
--- src/components/Game/useGame.ts
+++ src/components/Game/useGame.ts
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function useGame() {
   const [currentPlayer, setCurrentPlayer] = React.useState<"x" | "o">("x");
+  const [moveNumber, setMoveNumber] = React.useState(0);
+
+  const incrementMoveNumber = () => {
+    setMoveNumber(moveNumber + 1);
+  };
+
+  const decrementMoveNumber = () => {
+    setMoveNumber(moveNumber - 1);
+  };
 
   const switchPlayer = () => {
     if (currentPlayer === "x") {
@@ -13,5 +22,6 @@ export default function useGame() {
   return {
     currentPlayer,
     switchPlayer,
+    moveNumber,
   };
 }
