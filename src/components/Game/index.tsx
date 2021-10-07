96ff1d1e895c2d78e08dde6c18475c9b55ab1c00
Ivan Boban <ivan.boban911@gmail.com>
2021-10-07 19:11:45 +0200

    Added current user switching control

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Board from "../Board";
 import "./Game.css";
+import useGame from "./useGame";
 
 export default function Game() {
+  const { currentPlayer } = useGame();
   //to do map out squares onto game
-  return <div id="game">Game component</div>;
+  return (
+    <div id="game">
+      <h3>Current player: {currentPlayer} </h3>
+      <Board />
+    </div>
+  );
 }
--- src/components/Game/useGame.ts
+++ src/components/Game/useGame.ts
@@ -1,1 +1,17 @@
-export default function useGame() {}
+import React from "react";
+
+export default function useGame() {
+  const [currentPlayer, setCurrentPlayer] = React.useState<"x" | "o">("x");
+
+  const switchPlayer = () => {
+    if (currentPlayer === "x") {
+      setCurrentPlayer("o");
+      return;
+    }
+    setCurrentPlayer("x");
+  };
+  return {
+    currentPlayer,
+    switchPlayer,
+  };
+}
